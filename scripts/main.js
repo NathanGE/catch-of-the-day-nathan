cdb23886626fa20056c1b18f9c4e782889ea9b89
NathanGE <nathangelliott@gmail.com>
2016-03-16 08:53:37 -0400

    Required React and ReactDOM and created a store picker component

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-alert("Hello World!");
+var React = require('react');
+var ReactDOM = require('react-dom')
+
+/*
+  StorePicker component
+  This will let us make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+  
+  render : function() {
+    return (
+      <p>hello</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
