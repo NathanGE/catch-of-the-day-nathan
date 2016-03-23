f8815efd062f758f0ef38dfa86183149eebcb070
NathanGE <nathangelliott@gmail.com>
2016-03-22 21:09:31 -0400

    Used helper functions for dynamic defualt names in StorePicker

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var h = require('./helpers');
+
 /*
   App
 */
@@ -90,7 +92,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" ref="storeId" />
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
     )
