a1896c1d9737af8dae4ce28335f9f093d66c8600
NathanGE <nathangelliott@gmail.com>
2016-06-17 16:12:21 -0400

    Removed unneeded imports from main.js and updated createBrowserHistory to createHistory

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,18 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
-var createBrowserHistory = require('history/lib/createBrowserHistory');
-
-/*
-  Import Components
-*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route} from 'react-router';
+import  { createHistory } from 'history';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
-import Header from './components/Header';
-import Fish from './components/Fish';
-import AddFishFrom from './components/AddFishForm';
-import Inventory from './components/Inventory';
-import Order from './components/Order';
 
 /*
   Routes
 */
 
 var routes = (
-  <Router history={createBrowserHistory()}>
+  <Router history={createHistory()}>
     <Route path="/" component={StorePicker}/>
     <Route path="/store/:storeId" component={App}/>
     <Route path="*" component={NotFound}/>
