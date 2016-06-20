8a884d86426a5588c2301d5aef6361b1a4c5b7d5
NathanGE <nathangelliott@gmail.com>
2016-06-20 15:29:40 -0400

    Updated AddFishForm, Fish, and Order components to ES6 class syntax

--- scripts/components/AddFishForm.js
+++ scripts/components/AddFishForm.js
@@ -4,9 +4,11 @@
 */
 
 import React from 'react';
+import autobind from 'autobind-decorator';
 
-var AddFishForm = React.createClass({
-  createFish : function(event) {
+@autobind
+class AddFishForm extends React.Component {
+  createFish(event) {
     // Stop the form from submitting
     event.preventDefault();
     // Take the data from the form
@@ -20,8 +22,9 @@ var AddFishForm = React.createClass({
     // Add the fish to the App State
     this.props.addFish(fish);
     this.props.fishForm.reset();
-  },
-  render : function() {
+  }
+
+  render() {
     return (
       <form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
         <input type="text" ref="name" placeholder="Fish Name"/>
@@ -36,6 +39,7 @@ var AddFishForm = React.createClass({
       </form>
     )
   }
-});
+
+};
 
 export default AddFishForm;
--- scripts/components/Fish.js
+++ scripts/components/Fish.js
@@ -22,7 +22,7 @@ class Fish extends React.Component{
     var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
     return (
       <li className="menu-fish">
-        <img src={details.image} alt="detials.name" />
+        <img src={details.image} alt="details.name" />
         <h3 className="fish-name">
           {details.name}
           <span className="price">{h.formatPrice(details.price)}</span>
