4a165ebacb0c23b46c966c76c293c0f5aec38a25
NathanGE <nathangelliott@gmail.com>
2016-06-21 16:44:47 -0400

    Updated build to check error

--- scripts/components/Inventory.js
+++ scripts/components/Inventory.js
@@ -43,7 +43,7 @@ class Inventory extends React.Component {
   authHandler(err, authData) {
     console.log("In the auth handler");
     if(err) {
-      console.err(err);
+      console.error(err);
       return;
     }
 
