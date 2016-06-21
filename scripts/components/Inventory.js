86e0521fc72dccb88f2ab664d15835243d4605b4
NathanGE <nathangelliott@gmail.com>
2016-06-21 12:45:28 -0400

    Added login and logout button to Inventory component

--- scripts/components/Inventory.js
+++ scripts/components/Inventory.js
@@ -6,9 +6,79 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 import autobind from 'autobind-decorator';
+import Firebase from 'firebase';
+const ref = new Firebase('https://catch-of-the-day-nathan.firebaseio.com/');
 
 @autobind
 class Inventory extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      uid : ''
+    }
+  }
+
+  authenticate(provider) {
+    console.log("provider auth " + provider);
+    ref.authWithOAuthPopup(provider, this.authHandler);
+  }
+
+  componentWillMount() {
+    console.log("Checking if we can log them in");
+    var token = localStorage.getItem('token');
+    if(token) {
+      ref.authWithCustomToken(token, this.authHandler);
+    }
+  }
+
+  logout() {
+    ref.anauth();
+    localStorage.removeItem('token');
+    this.setState({
+      uid : null
+    });
+  }
+
+  authHandler(err, authData) {
+    console.log("In the auth handler");
+    if(err) {
+      console.err(err);
+      return;
+    }
+
+    // save the login token in browser
+    localStore.setItem('token', authData.token);
+
+    console.log(this.props.params.storeId);
+    const storeRef = ref.child(this.props.params.storeID);
+    storeRef.on('value', (snapshot) => {
+      var data = snapshot.val() || {};
+
+      if(!data.owner) {
+        // claim it as our own if no owner
+        storeRef.set({
+          owner : authData.uid
+        })
+      }
+      // update state to reflect current owner and user
+      this.setState({
+        uid : authData.uid,
+        owner : data.owner || authData.uid
+      });
+    });
+
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign in to manage your store's inventory</p>
+        <button className="facebook" onClick={this.authenticate.bind(this, 'facebook')}>Log In with Facebook</button>
+      </nav>
+    )
+  }
 
   renderInventory(key) {
     var linkState = this.props.linkState;
@@ -29,9 +99,28 @@ class Inventory extends React.Component {
   }
 
   render() {
+    let logoutButton = <button onClick={this.logout}>Log Out!</button>
+
+    // Check if user is logged in
+    if(!this.state.uid) {
+      return (
+        <div>{this.renderLogin()}</div>
+      )
+    }
+    // check they are the store owner
+    if(this.state.uid !== this.state.owner){
+      return (
+        <div>
+          <p>Sorry, you are not the store owner</p>
+          {logoutButton}
+        </div>
+      )
+    }
+
     return (
       <div>
         <h2>Inventory</h2>
+        {logoutButton}
         {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
